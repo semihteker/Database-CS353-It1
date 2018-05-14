008b50982cd3caf193e7887a54bffdc965821b74
Berkay Beken <berkaybekenn@gmail.com>
2018-05-14 20:34:04 +0300

    added database

--- setup/product.py
+++ setup/product.py
@@ -46,5 +46,47 @@ cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, porti
 cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
             VALUES (2, 'Pepsi', 'Drinks', 4, 200 )")
 
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'KingBurger', 'Burgers', 11, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'CheeseBurger', 'Burgers', 10, 200 )")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'FishFingers', 'Sneak',8 , 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'Nuggets', 'Sneak', 9, 270)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'DoubleBurger', 'Burgers', 14, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'DoubleChicken', 'Burgers', 12, 200 )")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'Coffee', 'Drinks', 6, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'Milkshake', 'IceCream', 5, 270)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'IceTea' , 'Drinks', 8, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (3, 'Sundae', 'IceCream', 4, 200 )")
+
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'Karakoy', 'Kahvalti', 11, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'Form', 'Kahvalti', 24, 200 )")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'PastirmaYumurta', 'Sahanda', 24, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'PatatesYumurta', 'Sahanda', 16, 270)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'KarisikOmlet', 'Yumurta', 5, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'KasarliOmlet', 'Yumurta', 10, 200 )")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'Profiterol', 'Desserts', 12, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'KestaneliPasta', 'Desserts', 9, 270)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'Limonata' , 'Drinks', 12, 150)")
+cursor.execute("INSERT INTO product (rest_id, food_name, food_type, price, portion)\
+            VALUES (4, 'Havuc', 'Drinks', 9, 200 )")
+
 conn.commit()
 conn.close()
