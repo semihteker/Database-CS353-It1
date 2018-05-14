2c05fad9d79f5d8a3d7b5fe5b5e99af086fa82c3
Unknown <cavdarhibrahim@gmail.com>
2018-05-14 14:39:31 +0300

    got rid of js

---
+++ static/js/addNewAddress.js
@@ -0,0 +1,16 @@
+// $(function(){
+// 	$('#button_save').click(function(){
+//
+// 		$.ajax({
+// 			url: '/addNewAddress',
+// 			data: $('form').serialize(),
+// 			type: 'POST',
+// 			success: function(response){
+// 				console.log(response);
+// 			},
+// 			error: function(error){
+// 				console.log(error);
+// 			}
+// 		});
+// 	});
+// });
