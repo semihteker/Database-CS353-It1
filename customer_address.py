bc3824fa7c777930cbbbca5211368c00d0b861a8
Berkay Beken <berkaybekenn@gmail.com>
2018-05-15 10:17:31 +0300

    fixed

--- customer_address.py
+++ customer_address.py
@@ -8,60 +8,60 @@ def xyz();
 
 
 
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (1, 'home', 'Cayyolu', 5, 'Ankara', 06554, 'Mesa')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (1, 'school', 'BilkentUni', 8, 'Ankara', 06000, '74thdormitory')")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number, street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (1, 'home', 5544667898,'Cayyolu', 5, 'Ankara', 06554, 'Mesa', 2)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (1, 'school', 5544647898, 'BilkentUni', 8, 'Ankara', 06000, '74thdormitory', 2)")
 
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (2, 'home', 'Yenimahalle', 3, 'Ankara', 07554, 'Toki')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (2, 'school', 'BilkentUni', 8, 'Ankara', 06000, EABuilding)")
-
-
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (3, 'home', 'Incek', 5, 'Ankara', 06554, 'Residences')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (3, 'school', 'BilkentUni', 8, 'Ankara', 06000, 'ABuilding')")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number, street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (2, 'home', 5224667898, 'Yenimahalle', 3, 'Ankara', 07554, 'Toki', 3)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (2, 'school', 5384667898, 'BilkentUni', 8, 'Ankara', 06000, EABuilding, 3)")
 
 
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (4, 'home', 'Umitkoy', 5, 'Ankara', 06876, 'Apartment')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (4, 'school', 'BilkentUni', 8, 'Ankara', 06000, 'BBuilding')")
-
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (5, 'home', 5389467543, 'Maslak', 2, 'Istanbul2, 34076, 'Apartment')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (5, 'work', 5389467543, 'KapaliCarsi',8, 'Istanbul', 34777, 'Carsi')")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (3, 'home', 5549997898, 'Incek', 5, 'Ankara', 06554, 'Residences', 2)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (3, 'school', 5544667777,'BilkentUni', 8, 'Ankara', 06000, 'ABuilding', 3)")
 
 
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (6, 'home', 'Bebek', 11, 'Istanbul', 34876, 'Home')")
-    ccursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (6, 'training', 'Florya', 2, 'Istanbul', 34854, 'GalatasarayTrainingPlace')")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (4, 'home', 5544668888, 'Umitkoy', 5, 'Ankara', 06876, 'Apartment', 1))
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (4, 'school', 5544669999,'BilkentUni', 8, 'Ankara', 06000, 'BBuilding', 3)")
 
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (7, 'home',  'Fulya', 7, 'Istanbul', 34336, 'Apartment')")
-    ccursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (7, 'stadium',  'Besiktas', 8, 'Istanbul', 34974, 'BesiktasStadium')")
-
-
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (8, 'home', 'Tarabya', 22, 'Istanbul', 34393, 'Apartment')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (8, 'training',  'Umraniye', 211, 'Istanbul', 34974, 'BesiktasTraining')")
-
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (9, 'home',  'Kadikoy', 17, 'Istanbul', 34982, 'Apartment')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (9, 'training',  'Umraniye', 92, 'Istanbul', 34875, 'BesiktasTraining')")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (5, 'home', 5387777543, 'Maslak', 2, 'Istanbul, 34076, 'Apartment', 20)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (5, 'work', 5388984543, 'KapaliCarsi',8, 'Istanbul', 34777, 'Carsi', 17)")
 
 
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (10, 'home', 'Yenikoy', 17, 'Istanbul', 34779, 'Apartment')")
-    cursor.execute("INSERT INTO customer_address (u_id, address_title, street_name,street_number, city, zipcode, address_desc)\
-                VALUES (10, 'training',  'Besiktas', 29, 'Istanbul', 34974, 'BesiktasStadium')")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (6, 'home', 5544698798, 'Bebek', 11, 'Istanbul', 34876, 'Home', 19)")
+    ccursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (6, 'training', 5539877898, 'Florya', 2, 'Istanbul', 34854, 'GalatasarayTrainingPlace', 19)")
+
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (7, 'home', 5598667898, 'Fulya', 7, 'Istanbul', 34336, 'Apartment', 18)")
+    ccursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (7, 'stadium', 5664667898, 'Besiktas', 8, 'Istanbul', 34974, 'BesiktasStadium', 17)")
+
+
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (8, 'home', 5567897898, 'Tarabya', 22, 'Istanbul', 34393, 'Apartment', 19)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (8, 'training', 5593089768,  'Umraniye', 211, 'Istanbul', 34974, 'BesiktasTraining', 17)")
+
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (9, 'home', 5544654673, 'Kadikoy', 17, 'Istanbul', 34982, 'Apartment', 16)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (9, 'training', 5544667232,  'Umraniye', 92, 'Istanbul', 34875, 'BesiktasTraining', 16)")
+
+
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (10, 'home', 5523678998, 'Yenikoy', 17, 'Istanbul', 34779, 'Apartment', 17)")
+    cursor.execute("INSERT INTO customer_address (u_id, address_title, phone_number,street_name,street_number, city, zipcode, address_desc, d_id)\
+                VALUES (10, 'training', 5447778899, 'Besiktas', 29, 'Istanbul', 34974, 'BesiktasStadium', 17)")
 
 
 
